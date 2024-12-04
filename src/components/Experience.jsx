14405043ddf40b691a288560c610e3f45e88eea9
Sky Randy <soras1758@gmail.com>
2024-12-04 09:40:17 +0900

    add experience page with minimum setup.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,23 +13,23 @@ function InputField(){
     return (
         <div className="experience-input">
             <div className="company">
-                <label htmlFor="">Company</label>
+                <label htmlFor="">Company: </label>
                 <input type="text" />
             </div>
             <div className="responsibilites">
-                <label htmlFor="">Responsibilities</label>
+                <label htmlFor="">Responsibilities: </label>
                 <input type="text" />
             </div>
             <div className="positionTitle">
-                <label htmlFor="">Position Title</label>
+                <label htmlFor="">Position Title: </label>
                 <input type="text" />
             </div>
             <div className="startDate">
-                <label htmlFor="">Start Date</label>
+                <label htmlFor="">Start Date: </label>
                 <input type="text" />
             </div>
             <div className="endDate">
-                <label htmlFor="">End Date</label>
+                <label htmlFor="">End Date: </label>
                 <input type="text" />
             </div>
 
